0122e12c82e2ef808a1c195520e4608b5c110d6e
banjotakahiro <watawata20220404@gmail.com>
2024-10-21 12:31:18 +0900

    課題の修正

--- index.php
+++ index.php
@@ -35,12 +35,12 @@ $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>PDO - SELECT</title>
 </head>
 <body>
-    <h2>データの抽出成功</h2>
+    <h2>本日のご紹介ペット！</h2>
     <form method="GET" action="">
         <input type="text" name="keyword" placeholder="検索キーワードを入力">
         <button type="submit">検索</button>
     </form>
-    <ul>
+    <p>
         <?php foreach ($animals as $animal): ?>
             <li><?= h($animal['type']) . 'の' . h($animal['classification']) . 'ちゃん' ?></li>
             <li style="list-style-type: none;"><?= h($animal['description']) ?></li>
@@ -48,7 +48,7 @@ $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <li style="list-style-type: none;"><?= '出身地：' . h($animal['birthplace'])  ?></li>
             <hr color="gray" size="2">
         <?php endforeach; ?>
-    </ul>
+    </p>
 </body>
 </html>
 
